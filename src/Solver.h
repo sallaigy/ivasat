4b82d3e76139b4b2e6e4cc4b6a239c67cc2c99c9
Gyula Sallai <salla016@gmail.com>
2023-04-30 17:16:22 +0200

    Refactor and simplify Solver

--- src/Solver.h
+++ src/Solver.h
@@ -1,14 +1,17 @@
 #ifndef IVA_SAT_SOLVER_H
 #define IVA_SAT_SOLVER_H
 
+#include "ivasat/ivasat.h"
+
 #include <vector>
 #include <algorithm>
 #include <cassert>
-
+#include <queue>
 
 namespace ivasat
 {
 
+/// Represents a literal inside of a SAT problem, that is a pair of (variable, value).
 class Literal
 {
 public:
@@ -44,7 +47,6 @@ public:
     return mData < 0;
   }
 
-
   [[nodiscard]] Literal negate() const
   {
     return Literal(-mData);
@@ -66,7 +68,7 @@ public:
   Clause(const Clause&) = default;
   Clause& operator=(const Clause&) = default;
 
-  Literal& operator[](int index)
+  const Literal& operator[](int index) const
   {
     return mLiterals[index];
   }
@@ -81,7 +83,7 @@ public:
   {
     return mLiterals.end();
   }
-  unsigned size() const
+  size_t size() const
   {
     return mLiterals.size();
   }
@@ -97,9 +99,133 @@ enum class Tribool
   Unknown
 };
 
-Tribool operator&(Tribool left, Tribool right);
-Tribool operator|(Tribool left, Tribool right);
 Tribool operator~(Tribool value);
+
+inline Tribool liftBool(bool value)
+{
+  return value ? Tribool::True : Tribool::False;
+}
+
+class Solver
+{
+  enum class ClauseStatus
+  {
+    Satisfied,
+    Conflicting,
+    Unit,
+    Unresolved
+  };
+
+  struct Statistics
+  {
+    unsigned checkedStates = 0;
+    unsigned checkedFullCombinations = 0;
+    unsigned learnedClauses = 0;
+  };
+
+  static constexpr int UnknownIndex = -1;
+
+public:
+  explicit Solver(const Instance& instance);
+
+  Status check();
+
+  std::vector<bool> model() const;
+
+  const Statistics& statistics() const
+  {
+    return mStats;
+  }
+
+  void assignUnitClause(Literal literal, int clauseIndex);
+
+  void assignVariable(Literal literal);
+
+  void undoAssignment(size_t variableIndex);
+
+  bool propagate();
+
+  void pushDecision(Literal literal);
+
+  void popDecision();
+
+  /// Pop the decision level which assigned `varIdx` and all decisions following it.
+  void popDecisionUntil(int varIdx);
+
+  Literal unassignedLiteral(const Clause& clause)
+  {
+    for (Literal literal : clause) {
+      auto currentValue = mVariableState[literal.index()];
+      if (currentValue == Tribool::Unknown) {
+        return literal;
+      }
+    }
+
+    assert(false && "Should be unreachable!");
+    return Literal{0};
+  }
+
+private:
+  bool isValidChoice(int index, bool value)
+  {
+    return std::ranges::none_of(mTrail, [&](Literal lit) {
+      return lit.index() == index && lit.value() != value;
+    });
+  }
+
+  void preprocess();
+
+  ClauseStatus checkClause(const Clause& clause);
+
+  void analyzeConflict(size_t trailIndex);
+
+  // Some helper methods
+  //==----------------------------------------------------------------------==//
+  size_t decisionLevel() const
+  {
+    return mDecisions.size();
+  }
+
+  size_t numAssigned() const
+  {
+    assert(std::ranges::count_if(mVariableState, [](auto& v) { return v != Tribool::Unknown; }) == mTrail.size());
+    return mTrail.size();
+  }
+
+  int pickDecisionVariable(int start) const;
+
+  // Debug methods
+  //==----------------------------------------------------------------------==//
+
+  /// Write the current implication graph to standard output in DOT format. If the conflicting clause index is not
+  /// `UnknownIndex`, a conflict node will be present in the graph as well.
+  void dumpImplicationGraph(int conflictClauseIndex = UnknownIndex);
+
+  // Fields
+  //==----------------------------------------------------------------------==//
+
+  // Clause database
+  std::vector<Clause> mClauses;
+
+  // Internal solver state
+  std::vector<Tribool> mVariableState;
+  std::vector<Literal> mDecisions;
+
+  // For each assigned variable index, the index of the variable and clause that implied its value.
+  // The value for decided and unassigned variables is going to be -1.
+  std::vector<int> mImplications;
+
+  // For each assigned variable index, the decision level at which it was assigned to a value.
+  // For unassigned variables, the value is going to be -1.
+  std::vector<int> mAssignedAtLevel;
+
+  // List of assignments in chronological order.
+  std::vector<Literal> mTrail;
+  std::vector<size_t> mTrailIndices;
+
+  Statistics mStats;
+};
+
 
 } // namespace ivasat
 
--- src/SolverUtils.cpp
+++ src/SolverUtils.cpp
@@ -1,32 +1,9 @@
 #include "Solver.h"
 
+#include <iostream>
+#include <sstream>
+
 using namespace ivasat;
-
-Tribool ivasat::operator&(Tribool left, Tribool right)
-{
-  if (left == Tribool::False || right == Tribool::False) {
-    return Tribool::False;
-  }
-
-  if (left == Tribool::Unknown || right == Tribool::Unknown) {
-    return Tribool::Unknown;
-  }
-
-  return Tribool::True;
-}
-
-Tribool ivasat::operator|(Tribool left, Tribool right)
-{
-  if (left == Tribool::True || right == Tribool::True) {
-    return Tribool::True;
-  }
-
-  if (left == Tribool::Unknown || right == Tribool::Unknown) {
-    return Tribool::Unknown;
-  }
-
-  return Tribool::False;
-}
 
 Tribool ivasat::operator~(Tribool value)
 {
@@ -39,4 +16,58 @@ Tribool ivasat::operator~(Tribool value)
   }
 
   return Tribool::Unknown;
+}
+
+std::ostream& ivasat::operator<<(std::ostream& os, Status status)
+{
+  switch (status) {
+    case Status::Sat:
+      os << "Sat";
+      break;
+    case Status::Unsat:
+      os << "Unsat";
+      break;
+    case Status::Unknown:
+      os << "Unknown";
+      break;
+  }
+
+  return os;
+}
+
+void Solver::dumpImplicationGraph(int conflictClauseIndex)
+{
+  const auto& conflictClause = mClauses[conflictClauseIndex];
+  std::stringstream ss;
+
+  ss << "digraph G {\n";
+
+  for (Literal lit : mTrail) {
+    int varIdx = lit.index();
+
+    int assignedAt = mAssignedAtLevel[varIdx];
+    ss << "node_" << varIdx << " [label=\"" << varIdx << ":" << std::boolalpha << lit.value() << "@" << assignedAt << "\"];\n";
+  }
+
+  for (int i = 0; i < mImplications.size(); ++i) {
+    int clauseIdx = mImplications[i];
+
+    if (clauseIdx == UnknownIndex) {
+      continue;
+    }
+
+    for (Literal lit : mClauses[clauseIdx]) {
+      if (lit.index() != i) {
+        ss << "node_" << lit.index() << " -> " << "node_" << i << "[label=\"  " << clauseIdx << "\"];\n";
+      }
+    }
+  }
+
+  for (Literal lit : conflictClause) {
+    ss << "node_" << lit.index() << " -> " << "conflict[label=\"" << conflictClauseIndex << "\"];\n";
+  }
+
+  ss << "}";
+
+  std::cout << ss.str() << std::endl;
 }
